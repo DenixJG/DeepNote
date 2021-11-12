962cc70c83046b132134f737c4616026650eed0a
Rafael Popescu <rafael.popescu140@gmail.com>
2021-11-12 16:36:20 +0100

    Added User and Note models

--- Models/Note.cs
+++ Models/Note.cs
@@ -6,7 +6,50 @@ using System.Threading.Tasks;
 
 namespace DeepNote.Models
 {
-    class Note
+    public class Note
     {
+        public User User {get; set;}
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public bool IsCompleted { get; set; }
+        public DateTime date { get; set; }
+
+        /// <summary>
+        /// Constructor de la clase <see cref="Note"/>
+        /// </summary>
+        public Note()
+        {
+            IsCompleted = false;
+            this.date = DateTime.Now;
+        }
+
+        /// <summary>
+        /// Constructor de la clase <see cref="Note"/>
+        /// </summary>
+        /// <param name="user"></param>
+        /// <param name="title"></param>
+        public Note(User user, string title)
+        {
+            User = user;
+            Title = title;
+            IsCompleted = false;
+            this.date = DateTime.Now;
+        }
+
+        /// <summary>
+        /// Constructor de la clase <see cref="Note"/>
+        /// </summary>
+        /// <param name="user"></param>
+        /// <param name="title"></param>
+        /// <param name="body"></param>
+        public Note(User user, string title, string body)
+        {
+            User = user;
+            Title = title;
+            Body = body;
+            IsCompleted = false;
+            this.date = DateTime.Now;
+        }
+
     }
 }
--- Models/User.cs
+++ Models/User.cs
@@ -6,7 +6,24 @@ using System.Threading.Tasks;
 
 namespace DeepNote.Models
 {
-    class User
+    public class User
     {
+        public string Username { get; set; }
+        public string Email {get; set;}
+        public string Password { get; set; }
+
+        /// <summary>
+        /// Constructor de la clase <see cref="User"/>
+        /// </summary>
+        /// <param name="username"></param>
+        /// <param name="email"></param>
+        /// <param name="password"></param>
+        public User(string username, string email, string password)
+        {
+            Username = username ?? throw new ArgumentNullException(nameof(username));
+            Email = email ?? throw new ArgumentNullException(nameof(email));
+            Password = password ?? throw new ArgumentNullException(nameof(password));
+        }
+
     }
 }
